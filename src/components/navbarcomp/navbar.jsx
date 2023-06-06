f9bb16b1fc50f5f7ac32e8f711824aea1793262d
OpenSourcePundit <shashank00098@gmail.com>
2023-06-07 00:35:53 +0530

    refactor: navbar,listingpage

--- src/components/navbarcomp/navbar.jsx
+++ src/components/navbarcomp/navbar.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 
- import "./navbar.css";
-import booklogo from "../../biblioteclogo.png" 
- import { Link,useNavigate } from "react-router-dom";
- import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
- import { faHeart,faCartShopping,faUser,faBars} from '@fortawesome/free-solid-svg-icons'
- import { useContext } from "react";
- import {DataContext} from "../../Contexts/dataContext"
-
+import "./navbar.css";
+import booklogo from "../../biblioteclogo.png";
+import { Link, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHeart,
+  faCartShopping,
+  faUser,
+  faBars,
+} from "@fortawesome/free-solid-svg-icons";
+import { useContext } from "react";
+import { DataContext } from "../../Contexts/dataContext";
 
 // import { useCartContext,useFilter,useWishListContext } from "../../context";
 export function Navbar() {
@@ -18,7 +22,7 @@ export function Navbar() {
 
   // const itemInCartReducer = (prev,curr)=> prev+curr.qty;
   // const totalItemsInCart = cartProducts.cart.reduce(itemInCartReducer,0)
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   // const navigate = useNavigate();
   // let token = localStorage.getItem("token");
@@ -26,44 +30,49 @@ export function Navbar() {
   //   localStorage.removeItem("token");
   //   navigate("/");
   // };
-  const{products,dataDispatch}=useContext(DataContext)  ;
-  const handleSearch = (value) =>{
+  const { products, dataDispatch, cartLength, wishlistLength } =
+    useContext(DataContext);
+  const handleSearch = (value) => {
     navigate("/products");
-    dataDispatch(
-      {type:"HandleSearch",payload:value}
-    )
-    
-  }
+    dataDispatch({ type: "HandleSearch", payload: value });
+  };
 
   return (
     <>
-
-
-    <nav className="navbar">
-      <div className="logo">
-        <Link  to="/"><img src={booklogo} alt="bibliotec" srcset="" /></Link>
-      </div>
-      <div className="searchbox">
-        <form>
-          <input type="text" placeholder="Search here" onChange={(e)=>{handleSearch(e.target.value)}}/>
-        </form>
-      </div>
-      <div className="icons">
-        <Link  to="/wishlist" className="nav-icons"><FontAwesomeIcon icon={faHeart} /></Link>
-        <Link  to="/cart" className="nav-icons"><FontAwesomeIcon icon={faCartShopping} /></Link>
-        <Link  to="/profile" className="nav-icons"><FontAwesomeIcon icon={faUser} /></Link>
-        <Link  to="" className="nav-icons"><FontAwesomeIcon icon={faBars} /></Link>
-      </div>
+      <nav className="navbar">
+        <div className="logo">
+          <Link to="/">
+            <img src={booklogo} alt="bibliotec" srcset="" />
+          </Link>
+        </div>
+        <div className="searchbox">
+          <form>
+            <input className="input"
+              type="text"
+              placeholder="Search here"
+              onChange={(e) => {
+                handleSearch(e.target.value);
+              }}
+            />
+          </form>
+        </div>
+        <div className="icons">
+          <Link to="/wishlist" className="nav-icons">
+            <FontAwesomeIcon icon={faHeart} />{" "}
+            <span className="lengthItem">{wishlistLength}</span>{" "}
+          </Link>
+          <Link to="/cart" className="nav-icons">
+            <FontAwesomeIcon icon={faCartShopping} />{" "}
+            <span className="lengthItem">{cartLength}</span>
+          </Link>
+          <Link to="/profile" className="nav-icons">
+            <FontAwesomeIcon icon={faUser} />
+          </Link>
+          <h3 className="nav-icons">
+            {localStorage.getItem("name")}
+          </h3>
+        </div>
       </nav>
-      {/* <div className="links">
-        <a href="/cart"> Cart</a><br/>
-        <a href="/wishlist">Wishlist</a><br/>
-        <a href="/orders">Orders</a><br/>
-        <a href="/address">Addresses</a><br/>
-        <a href="/login"><FontAwesomeIcon icon={faUser} /> Login</a><br/>
-      </div> */}
-      </>
-    
+    </>
   );
-};
-
+}
--- src/pages/product/productlistingpage/components/productcard/productcard.jsx
+++ src/pages/product/productlistingpage/components/productcard/productcard.jsx
@@ -1,54 +1,168 @@
-import {React} from 'react'
-import {useNavigate} from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart,faCartShopping,faUser,faBars,faStar} from '@fortawesome/free-solid-svg-icons'
+import { React, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { AiFillHeart, AiFillStar } from "react-icons/ai";
+import { FaShoppingCart } from "react-icons/fa";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { DataContext } from "../../../../../Contexts/dataContext";
 
 import "./productcard.css";
 
-export const ProductCard = ({prod}) =>{     
-    const navigate = useNavigate();
+export const ProductCard = ({ prod }) => {
+  const navigate = useNavigate();
+  let result = {};
+  let result1 = {};
+  let { dataDispatch, cart, wishlist } = useContext(DataContext);
 
-    const productdetailsclickHandler = () =>{
-        //console.log(e.target.value);
-        console.log(prod);
-        navigate(`./${prod._id}`);        
+  const productdetailsclickHandler = () => {
+    navigate(`./${prod._id}`);
+  };
+
+  const isInCart = cart.some((item) => item._id === prod._id);
+  const {
+    _id,
+    title,
+    author,
+    image,
+    price,
+    newPrice,
+    discount,
+    rating,
+    Language,
+  } = prod;
+
+  const handleAddCart = async (prod) => {
+    try {
+      const prod1 = {
+        product: prod,
+      };
+      const encodedToken = localStorage.getItem("encodedToken");
+      const response = await fetch(`/api/user/cart`, {
+        method: "POST",
+        headers: {
+          authorization: encodedToken,
+        },
+        body: JSON.stringify(prod1),
+      });
+      // saving the encodedToken in the localStorage
+      result = await response.json();
+      console.log(result.cart);
+      dataDispatch({
+        type: "handleCart",
+        payload: result.cart,
+      });
+    } catch (error) {
+      console.log(error);
     }
-    return(
-        // <div className="container">
-            <div className="card" onClick={productdetailsclickHandler}>
+  };
 
-                <div className="img">
-                    <img src={prod.image} alt="" width="auto" height="200px" />
-                </div>
-                <div className="content">
-                    <div className="review-container">
-                        <div className="stars">
-                        <span>Reviews</span>
-                        {[1,2,3,4,5].map((i)=>
-                            i<=prod.rating ? < FontAwesomeIcon icon={faStar} style={{color: "#f08c00",}} />
-                            :<FontAwesomeIcon icon={faStar} />                        
-                        )}
-                        </div>                        
-                    </div>
-                    <h5 className="price">Rs.{prod.newPrice}</h5>
-                    <div className="name">{prod.title}</div>
-                    <div className="description">
-                        <p>Author:{prod.author}</p>
-                        <p>Category{prod.categoryName}</p>
-                        <p>Original Price: Rs.{prod.price}</p>
-                    </div>
-                    <div className="button-container">                        
-                        <button className="cart-button">
-                            <span>Add To Cart<FontAwesomeIcon icon={faCartShopping} /></span>
-                        </button>  
-                <button className="wishlist-button"> <FontAwesomeIcon icon={faHeart}/></button>
+  const isInWishlist = wishlist.some((item) => item._id === prod._id);
 
-                    </div>
-                </div>
+  const handleWishlist = async (prod) => {
+    try {
+      const encodedToken = localStorage.getItem("encodedToken");
+      const response = await fetch(
+        isInWishlist ? `/api/user/wishlist/${prod._id}` : `/api/user/wishlist`,
+        {
+          method: isInWishlist ? "DELETE" : "POST",
+          headers: {
+            authorization: encodedToken,
+          },
+          body: isInWishlist ? "" : JSON.stringify({ product: prod }),
+        }
+      );
+      // saving the encodedToken in the localStorage
+      result1 = await response.json();
 
-            </div>
-        // </div>
-    )
-}
+      dataDispatch({
+        type: "handleWishlist",
+        payload: result1.wishlist,
+      });
+      console.log("wishlist", wishlist, "productid", prod._id);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  return (
+    // <div className="container">
+    <div className="card" onClick={productdetailsclickHandler}>
+      <div className="img">
+        <img src={prod.image} alt="" width="auto" height="200px" />
+      </div>
 
+      <div className="content">
+        <div className="review-container">
+          <div className="stars">
+            <span>Reviews</span>
+            {[1, 2, 3, 4, 5].map((i) =>
+              i <= prod.rating ? (
+                <FontAwesomeIcon icon={faStar} style={{ color: "#f08c00" }} />
+              ) : (
+                <FontAwesomeIcon icon={faStar} />
+              )
+            )}
+          </div>
+        </div>
+        <h5 className="price">Rs.{prod.newPrice}</h5>
+        <div className="name">{prod.title}</div>
+        <div className="description">
+          <p>Author:{prod.author}</p>
+          <p>Category{prod.categoryName}</p>
+          <p>Original Price: Rs.{prod.price}</p>
+        </div>
+      </div>
+      <div className="button-container">
+        <div className="cart-button">
+          {isInCart ? (
+            <button
+              className="btn"
+              style={{}}
+              onClick={() => navigate("/cart")}
+            >
+              <FaShoppingCart />
+              <span> Go to Cart </span>
+            </button>
+          ) : (
+            <button
+              className="btn"
+              style={{}}
+              onClick={() => handleAddCart(prod)}
+            >
+              <FaShoppingCart />
+              <span> Add to Cart</span>
+            </button>
+          )}
+        </div>
+        {/* <button className="cart-button">
+            <span>
+              Add To Cart
+              <FontAwesomeIcon icon={faCartShopping} />
+            </span>
+          </button> */}
+
+        {/* <button className="wishlist-button">
+            {" "}
+            <FontAwesomeIcon icon={faHeart} />
+          </button> */}
+        <div className="wishlist-button">
+          {isInWishlist ? (
+            <button
+              className="remove-heart"
+              onClick={() => handleWishlist(prod)}
+            >
+              {" "}
+              <AiFillHeart />{" "}
+            </button>
+          ) : (
+            <button className="add-heart" onClick={() => handleWishlist(prod)}>
+              {" "}
+              <AiFillHeart />{" "}
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+    // </div>
+  );
+};
